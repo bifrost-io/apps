614b656989faf3d9fd01d32334dcb7817ffcba79
janpo <janpo.pan@gmail.com>
2021-07-14 17:23:39 +0800

    update types

--- packages/apps-config/src/api/spec/bifrost-asgard.ts
+++ packages/apps-config/src/api/spec/bifrost-asgard.ts
@@ -150,6 +150,7 @@ const definitions: OverrideBundleDefinition = {
           currency_id: 'CurrencyId',
           token_pool: 'Balance',
           vstoken_pool: 'Balance',
+          token_ceiling: 'Balance',
           token_base_supply: 'Balance',
           vstoken_base_supply: 'Balance'
         }
