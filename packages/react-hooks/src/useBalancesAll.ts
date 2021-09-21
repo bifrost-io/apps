b4bf2b117ca1ac4ccf30f0b52f292c5cef5332b0
janpo <janpo.pan@gmail.com>
2021-09-22 00:05:39 +0800

    update vesting

--- packages/react-hooks/src/useBalancesAll.ts
+++ packages/react-hooks/src/useBalancesAll.ts
@@ -1,7 +1,11 @@
 // Copyright 2017-2021 @polkadot/react-hooks authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
+/* eslint-disable */
+
 import type { DeriveBalancesAll } from '@polkadot/api-derive/types';
+
+import { useBestNumber } from '@polkadot/react-hooks';
 
 import { useApi } from './useApi';
 import { useCall } from './useCall';
@@ -12,8 +16,51 @@ import { useCall } from './useCall';
  * @param accountAddress The account address of which balance is to be returned
  * @returns full information about account's balances
  */
-export function useBalancesAll (accountAddress: string): DeriveBalancesAll | undefined {
+export function useBalancesAll(accountAddress: string): DeriveBalancesAll | undefined {
   const { api } = useApi();
+  const bestNumber = useBestNumber();
 
-  return useCall<DeriveBalancesAll>(api.derive.balances?.all, [accountAddress]);
+  const vesting = api.registry.createType(
+    'VestingInfo',
+    useCall(api.query.vesting?.vesting, [accountAddress])?.toJSON()
+  );
+  const vestingStartAt = useCall(api.query.vesting?.vestingStartAt);
+  const emptyVest = api?.registry?.createType('VestingInfo');
+  const result = useCall<DeriveBalancesAll>(api.derive.balances?.all, [accountAddress]);
+
+  if (result) {
+    if (result.vestingLocked.isEmpty) return result;
+
+    const { locked: vestingTotal, perBlock, startingBlock: theStartingBlock } = vesting.isEmpty ? emptyVest : vesting;
+    let startingBlock = theStartingBlock;
+
+    // startingBlock = theStartingBlock.add(api.registry.createType('BlockNumber', 100000))
+    if (!vestingStartAt?.isEmpty) {
+      startingBlock = theStartingBlock.add(api.registry.createType('BlockNumber', vestingStartAt));
+    }
+
+    const isStarted = bestNumber?.gt(startingBlock);
+    const vestedNow = isStarted ? perBlock.mul(bestNumber.sub(startingBlock)) : new BN(0);
+    const vestedBalance = vestedNow.gt(vestingTotal)
+      ? vestingTotal
+      : api.registry.createType('Balance', vestedNow);
+    const isVesting = isStarted && !result?.vestingLocked.isZero();
+
+    result.isVesting = isVesting;
+    result.vestedBalance = vestedBalance;
+    result.vestedClaimable = api.registry.createType(
+      'Balance',
+      isVesting ? result?.vestingLocked.sub(vestingTotal.sub(vestedBalance)) : 0
+    );
+    result.vestingEndBlock = api.registry.createType(
+      'BlockNumber',
+      isVesting ? vestingTotal.div(perBlock).add(startingBlock) : 0
+    );
+    result.vestingPerBlock = perBlock;
+    result.vestingTotal = vestingTotal;
+
+    return result;
+  }
+
+  return result;
 }
